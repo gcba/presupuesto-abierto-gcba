2d6af62721dc5118eca778b317e49143b2a81b27
Lucas Fernandez <lfvillafranca@gmail.com>
2016-09-26 14:00:46 -0300

    Dependencias instaladas

---
+++ bower_components/bubbletree/demos/berlin/config.js
@@ -0,0 +1,8 @@
+var config = {
+	apiUrl: 'http://berlin.offenerhaushalt.de/api',
+	dataset: 'berlin',
+	drilldowns: ['hauptfunktion', 'oberfunktion'],
+	cuts: ['year:2011'],
+	localApiCache: 'aggregate.json',
+	bubbleType: 'plain'
+};
